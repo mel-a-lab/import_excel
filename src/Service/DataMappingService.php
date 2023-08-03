247d4bcf023771b9098cc4d59122b7171d0dd643
mel-a-lab <melanie.dussenne@gmail.com>
2023-08-03 12:06:27 +0200

    modif_code

--- src/Service/DataMappingService.php
+++ src/Service/DataMappingService.php
@@ -10,24 +10,33 @@ class DataMappingService
         foreach ($dataToMap as $key => $value) {
             $key = str_replace(['è', 'é'], 'e', $key);
             $key = str_replace('°', 'umero', $key);
+            dump($key);
+            $key = str_replace('compteaffaire', 'compte Affaire', $key);
             
-            $key = str_replace('compteaffaire', 'compteAffaire', $key);
-            
-            $key = str_replace('numeroEtNomDeLaVoie', 'numeroEtNomDeVoie', $key);
+            $key = str_replace('Numero et Nom de la voie', 'numero Et Nom De Voie', $key);
             $pattern = '/\([^)]*\)\s*/';
             $replacement = '';
 
             $key = preg_replace($pattern, $replacement, $key);
-
+         
             $words = explode(' ', strtolower($key));
+            
             $capitalizedWords = array_map('ucfirst', $words);
+     
             $pascalCaseString = implode('', $capitalizedWords);
+
             $pascalCaseString = lcfirst($pascalCaseString);
+
             $dataMapped[$pascalCaseString] = $value;
+
+
             unset($dataMapped['telephoneJob']);
             unset($dataMapped['compteaffaire']);
             unset($dataMapped['numeroEtNomDeLaVoie']);
+             
+       
         }
+        dump($dataMapped);
         return $dataMapped;
     }
 }
